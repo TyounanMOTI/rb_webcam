eea367aa87532c256c5e01e7ce9a97b441f68432
TyounanMOTI <tyounan.moti@gmail.com>
2010-03-12 20:26:28 +0900

    refactor Webcam.initialize to call 'open' method

--- lib/rb_webcam.rb
+++ lib/rb_webcam.rb
@@ -14,11 +14,15 @@ end
 
 class Webcam
   def initialize(camera_id=0)
+    open(camera_id)
+  end
+
+  def open(camera_id=0)
     @capture_handler = Highgui::cvCreateCameraCapture(camera_id)
   end
 
   def grab
-    Highgui::cvGrabFrame(@capture_handler)
+    Highgui.cvGrabFrame(@capture_handler)
   end
 end
 
