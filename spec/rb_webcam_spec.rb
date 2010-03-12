6e94cc166ae0ea4c64a9f69042d8fc67cff800ac
TyounanMOTI <tyounan.moti@gmail.com>
2010-03-12 19:07:27 +0900

    refactor spec by nesting 'descript'

--- spec/rb_webcam_spec.rb
+++ spec/rb_webcam_spec.rb
@@ -1,21 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe Webcam, "when given camera_id is 0" do
-  before do
-    @c_webcam = Webcam.new(0)
+describe Webcam do
+  context "when given camera_id is 0" do
+    subject { Webcam.new(0) }
+    it { should_not be_nil }
   end
 
-  it "should not be nil" do
-    @c_webcam.should_not be_nil
-  end
-end
-
-describe Webcam, "when grabs a frame" do
-  before do
-    @c_webcam = Webcam.new(0)
-  end
-
-  it "should succeed" do
-    @c_webcam.grab.should == 1
+  context "when grabs a frame" do
+    subject { Webcam.new.grab }
+    it "should succeed" do
+      should == 1
+    end
   end
 end
