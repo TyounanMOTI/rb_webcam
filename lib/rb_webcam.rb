abc7c06c5a7305376490e74acaa9db543ef6be8c
TyounanMOTI <tyounan.moti@gmail.com>
2010-03-13 17:07:46 +0900

    wrote comments at each methods

--- lib/rb_webcam.rb
+++ lib/rb_webcam.rb
@@ -18,25 +18,35 @@ module Highgui
 end
 
 class Webcam
+  # Call 'open(camera_id)'.
   def initialize(camera_id=0)
     open(camera_id)
   end
 
+  # Open camera with 'camera_id'. (default = 0)
+  # If specified '0', camera will be auto-detected.
   def open(camera_id=0)
     @capture_handler = Highgui::cvCreateCameraCapture(camera_id)
   end
 
+  # Open camera with 'method with block' sentence.
+  # This will open then close at start and end of block.
+  # ex.
+  # Webcam.open { |camera| @image = camera.grab }
   def self.open(camera_id=0)
     webcam = Webcam.new(camera_id)
     yield webcam
     webcam.close
   end
 
+  # Grab a frame from camera and returns a pointer to IplImage.
+  # This needs camera still opened.
   def grab
     raise "Camera has'nt be initialized" if @capture_handler.nil?
     Highgui.cvQueryFrame(@capture_handler)
   end
 
+  # Close camera. You need close opened camera for cleaner behavior.
   def close
     Highgui::cvReleaseCapture(FFI::MemoryPointer.new(:pointer).write_pointer(@capture_handler))
     @capture_handler = nil
