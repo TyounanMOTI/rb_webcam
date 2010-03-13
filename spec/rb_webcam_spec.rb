8040c3695210353b268efabcf247a70ddb44f39c
TyounanMOTI <tyounan.moti@gmail.com>
2010-03-13 16:55:56 +0900

    refactor spec using 'share_example_for' with "Webcam which closed"

--- spec/rb_webcam_spec.rb
+++ spec/rb_webcam_spec.rb
@@ -1,6 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Webcam do
+  share_examples_for "Webcam which closed" do
+    it { lambda{ @c_webcam.grab }.should raise_error(RuntimeError, "Camera has'nt be initialized") }
+  end
+
   context "when given camera_id is 0" do
     before(:all) do
      @c_webcam = Webcam.new(0)
@@ -10,7 +14,7 @@ describe Webcam do
     it { @c_webcam.capture_handler.should be_instance_of(FFI::Pointer) }
     it { @c_webcam.grab.should be_instance_of(FFI::Pointer) }
     it { @c_webcam.close.should be_nil }
-    it { lambda{ @c_webcam.grab }.should raise_error(RuntimeError, "Camera has'nt be initialized") }
+    it_should_behave_like "Webcam which closed"
   end
 
   context "when grab a frame using method with block" do
@@ -21,6 +25,6 @@ describe Webcam do
       end
     end
 
-    it { lambda{ @c_webcam.grab }.should raise_error(RuntimeError, "Camera has'nt be initialized") }
+    it_should_behave_like "Webcam which closed"
   end
 end
