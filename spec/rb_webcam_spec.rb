385ffe8d9d8ab92168ad0bd935dcf91c9f0d17c1
TyounanMOTI <tyounan.moti@gmail.com>
2010-03-13 16:12:07 +0900

    refactored spec

--- spec/rb_webcam_spec.rb
+++ spec/rb_webcam_spec.rb
@@ -2,34 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Webcam do
   context "when given camera_id is 0" do
-    before do
-      @c_webcam = Webcam.new(0)
+    before(:all) do
+     $c_webcam = Webcam.new(0)
     end
 
-    it { @c_webcam.should_not be_nil }
-    it { @c_webcam.capture_handler.should be_instance_of(FFI::Pointer) }
-
-    after do
-      @c_webcam.close
-    end
-  end
-
-  context "when grabs a frame" do
-    subject { Webcam.new.grab }
-    it { should == 1 }
-  end
-
-  context "when close a camera" do
-    subject { Webcam.new.close }
-    it { should be_nil }
-  end
-
-  context "when try to grab closed Webcam" do
-    before do
-      @webcam = Webcam.new
-      @webcam.close
-    end
-
-    it { lambda{ @webcam.grab }.should raise_error(RuntimeError, "Camera has'nt be initialized") }
+    it { $c_webcam.should_not be_nil }
+    it { $c_webcam.capture_handler.should be_instance_of(FFI::Pointer) }
+    it { $c_webcam.grab.should == 1 }
+    it { $c_webcam.close.should be_nil }
+    it { lambda{ $c_webcam.grab }.should raise_error(RuntimeError, "Camera has'nt be initialized") }
   end
 end
